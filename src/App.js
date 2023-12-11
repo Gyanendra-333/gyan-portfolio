1741ad5e05c201140b09325f0d4221dc5acab021
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-11 21:49:31 +0530

    start create app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./index.css";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+
 
 function App() {
   return (
-    <div >
-      <h1>this is App Page.</h1>
-    </div>
+    < >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 

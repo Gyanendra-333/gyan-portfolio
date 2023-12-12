d1eec60899fbae5fd75e17e9c50fed6121f8c5f2
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-12 13:58:11 +0530

    hero img

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,10 +1,25 @@
 import "./HeroImg.css";
+import React from 'react';
 
-import React from 'react'
+import IntroImg from "../assets/img01.jpg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
     return (
-        <div>HeroImg</div>
+        <div className="hero">
+
+            <div className="mask">
+                <img className="intro-img" src={IntroImg} alt="Intro Img" />
+            </div>
+
+            <div className="content">
+                <p>Hi , I Am A Frontend Developer.</p>
+                <h1>React Developer</h1>
+                <div>
+                    <Link to="/project" className="btn">Project</Link>
+                </div>
+            </div>
+        </div>
     )
 }
 

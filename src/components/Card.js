30a0f1ac5c88f0cf47a8186f73eaef85b733a349
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-13 23:01:29 +0530

    card

---
+++ src/components/Card.js
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import "./Card.css";
+
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="pricing">
+
+            <div className="card-container">
+
+                <div className="card">
+                    <h3>- Basic -</h3>
+                    <span className="bar"></span>
+                    <p className="btc">$ 99</p>
+                    <p>- 3 Days -</p>
+                    <p>- 2 Pages -</p>
+                    <p>- Features -</p>
+                    <p>- Responsive Design -</p>
+                    <Link to="/contact" className="btn">Join Now</Link>
+                </div>
+                <div className="card">
+                    <h3>- Premium -</h3>
+                    <span className="bar"></span>
+                    <p className="btc">$ 199</p>
+                    <p>- 5 Days -</p>
+                    <p>- 5 Pages -</p>
+                    <p>- Features -</p>
+                    <p>- Responsive Design -</p>
+                    <Link to="/contact" className="btn">Join Now</Link>
+                </div>
+                <div className="card">
+                    <h3>- Business -</h3>
+                    <span className="bar"></span>
+                    <p className="btc">$ 299</p>
+                    <p>- 7 Days -</p>
+                    <p>- 9 Pages -</p>
+                    <p>- Features -</p>
+                    <p>- Responsive Design -</p>
+                    <Link to="/contact" className="btn">Join Now</Link>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ProjectHeroImg from '../components/ProjectHeroImg';
+import Card from '../components/Card';
 
 const Project = () => {
     return (
         <div>
             <Navbar />
             <ProjectHeroImg heading="Projects" text="Some of My Most Recent Works." />
+            <Card />
             <Footer />
         </div>
     )

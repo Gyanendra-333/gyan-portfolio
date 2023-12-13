1e8ef7265285fd2ad3fb1410a88aedb6f3e8c8b0
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-13 14:59:10 +0530

    ProjectHeroImg

---
+++ src/components/ProjectHeroImg.js
@@ -0,0 +1,11 @@
+import "./ProjectHeroImg.css";
+
+import React from 'react'
+
+const ProjectHeroImg = () => {
+    return (
+        <div>ProjectHeroImg</div>
+    )
+}
+
+export default ProjectHeroImg;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ProjectHeroImg from '../components/ProjectHeroImg';
 
 const Project = () => {
     return (
         <div>
             <Navbar />
+            <ProjectHeroImg />
             <Footer />
         </div>
     )

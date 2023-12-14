80ab10520a02592585b1c84d75aaa0ebf9b3e777
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-14 13:54:54 +0530

    Work Card

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ProjectHeroImg from '../components/ProjectHeroImg';
 import Card from '../components/Card';
+import WorkCard from '../components/WorkCard';
 
 const Project = () => {
     return (
         <div>
             <Navbar />
             <ProjectHeroImg heading="Projects" text="Some of My Most Recent Works." />
+            <WorkCard />
             <Card />
             <Footer />
         </div>

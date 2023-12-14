da66fbdd489c94c895a8a3179d77691417371548
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-12-14 23:07:46 +0530

    about content

---
+++ src/components/AboutContent.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import "./AboutContent.css";
+import React1 from "../assets/img01.jpg";
+import React2 from "../assets/img01.jpg";
+
+
+import React from 'react'
+
+const AboutContent = () => {
+    return (
+        <div className="about">
+            <div className="left">
+                <h1>Who I am ?</h1>
+                <p>I am a React Front-End Developer. I Create Responsive secure Website for My Client.</p>
+                <Link to="/contact">
+                    <button className="btn">Contact me</button>
+                </Link>
+            </div>
+
+            <div className="right">
+                <div className="img-container">
+
+                    <div className="img-stack top">
+                        <img className="img" src={React1} alt="true" />
+                    </div>
+                    <div className="img-stack bottom">
+                        <img className="img" src={React2} alt="true" />
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutContent;
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import ProjectHeroImg from '../components/ProjectHeroImg'
+import AboutContent from '../components/AboutContent'
 
 const About = () => {
     return (
         <div>
             <Navbar />
             <ProjectHeroImg heading="About" text="I Am A Friendly Front-End Developer." />
+            <AboutContent />
             <Footer />
         </div>
     )

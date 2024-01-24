12606e9393a1c9017833bb31cb3cf2e92d1c85ec
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-25 01:03:28 +0530

    portfolio

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,5 +1,5 @@
 import "./WorkCard.css";
-import pro1 from "../assets/img02.png";
+// import pro1 from "../assets/img02.png";
 
 import React from 'react'
 import { NavLink } from "react-router-dom";
@@ -13,8 +13,8 @@ const WorkCard = (props) => {
             <div className="pro-details">
                 <p>{props.text} </p>
                 <div className="pro-btns">
-                    <NavLink to={props.view} className="btn">View</NavLink>
-                    <NavLink to="url.com" className="btn">Source</NavLink>
+                    <NavLink target="_blank" to={props.view} className="btn">View</NavLink>
+                    <NavLink target="_blank" to="url.com" className="btn">Source</NavLink>
                 </div>
             </div>
         </div>

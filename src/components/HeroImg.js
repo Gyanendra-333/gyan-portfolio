2025c120e588c4e932bb643452f610f720959a8b
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-25 00:35:17 +0530

    heroimg

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -13,7 +13,7 @@ const HeroImg = () => {
             </div>
 
             <div className="content">
-                <p>Hi , I am a Front-End Developer.</p>
+                <p>Hi , I am a Frontend Developer.</p>
                 <h1>React Developer</h1>
                 <div>
                     <Link to="/project" className="btn" >Project</Link>

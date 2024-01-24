12cb56e5c18730158670353f3ab0a821d73bb1f5
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-25 05:01:57 +0530

    projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,25 +1,47 @@
-import pro1 from "../assets/img01.jpg";
-import pro2 from "../assets/img01.jpg";
-import pro3 from "../assets/img01.jpg";
+import pro1 from "../assets/go-holidays.png";
+import pro2 from "../assets/academy.png";
+import pro3 from "../assets/e-commerce.png";
+import pro4 from "../assets/meme.png";
+import pro5 from "../assets/kitchen.png";
+import pro6 from "../assets/search.png";
+
 
 const ProjectCardData = [
     {
         imgsrc: pro1,
-        title: "E-Commerce Design",
-        text: "This is text about our website , and its given features",
-        view: "https://www.google.com/"
+        title: "Tour & Travel Web App",
+        text: "This is a React Js Project. In this project we use concept like Hooks, Props, DOM, State, Event Handling, Array Destructuring, React Router many more.",
+        view: "https://gyanendra-go-holidays.netlify.app/"
     },
     {
         imgsrc: pro2,
-        title: "E-Commerce Design",
-        text: "This is text about our website , and its given features",
-        view: "https://www.google.com/"
+        title: "Education Website",
+        text: "This is HTML CSS and JavaScript Project. While making this project we learn many JavaScript Concept like Functions, IIFE, Destructuring, Objects and many more.",
+        view: "https://gyanendra-future-academy.netlify.app/"
     },
     {
         imgsrc: pro3,
-        title: "E-Commerce Design",
-        text: "This is text about our website , and its given features",
-        view: "https://www.google.com/"
+        title: "E-Commerce Website ",
+        text: "This is HTML CSS and JavaScript Project. While making this project we learn many JavaScript Concept like Functions, IIFE, Destructuring, Objects and many more.",
+        view: "https://gyanendra-shop-hub-online.netlify.app/"
+    },
+    {
+        imgsrc: pro4,
+        title: "Meme Generator Web",
+        text: "This is Basic JavaScript Project.In this Project we learn Functions, Events, DOM, IIFE, many more.",
+        view: "https://free-meme-generator.netlify.app/"
+    },
+    {
+        imgsrc: pro5,
+        title: "Food Blog Project ",
+        text: "This is Completely Bootstrap Project. In this project we use many Bootstrap classes for design website and many bootstrap Components.",
+        view: "https://the-kitchen-home.netlify.app/"
+    },
+    {
+        imgsrc: pro6,
+        title: "Image Search Js Project",
+        text: "This is Basic JavaScript Project.In this Project we learn Functions, Events, DOM, IIFE, many more.",
+        view: "https://image-search-app-online.netlify.app/"
     }
 ]
 
